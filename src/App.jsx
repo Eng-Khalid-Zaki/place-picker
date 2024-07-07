0e6e69bd65be7ec1a5bc931ea3b14a49dd2f1ec0
Eng-Khalid-Zaki <eng.khalid.zaki.2022@gmail.com>
2024-07-07 04:41:09 +0300

    Update the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
 import Modal from "./components/Modal.jsx";
 import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
 import logoImg from "./assets/logo.png";
+import { sortPlacesByDistance } from "./loc.js";
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [sortedPlaces, setSortedPlaces] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      setSortedPlaces(sortPlacesByDistance(AVAILABLE_PLACES, lat, lon));
+    });
+  }, []);
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -28,6 +38,10 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+    const storedIds = JSON.parse(localStorage.getItem("selectedIds")) || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem("selectedIds", JSON.stringify([id, ...storedIds]));
+    }
   }
 
   function handleRemovePlace() {
@@ -35,6 +49,12 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedIds")) || [];
+    localStorage.setItem(
+      "selectedIds",
+      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
+    );
   }
 
   return (
@@ -63,8 +83,9 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
-          onSelectPlace={() => handleSelectPlace}
+          fallbackText={"Sorting places by distance..."}
+          places={sortedPlaces}
+          onSelectPlace={handleSelectPlace}
         />
       </main>
     </>
